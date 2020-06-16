cf5f46e7b7e37d51013e5bac1469d0827ecf5456
ilijan.militar <ilijan.militar@quantox.com>
2020-06-16 16:53:48 +0200

    adding controllers

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::resource('buyers', 'Buyer\BuyerController', ['only'=>['index','show']]);
+
+Route::resource('categories', 'Category\CategoryController', ['except'=>['create','edit']]);
+
+Route::resource('vendors', 'Vendor\VendorController', ['only'=>['index','show']]);
+
+Route::resource('sellers', 'Seller\SellerController', ['only'=>['index','show']]);
+
+Route::resource('transactions', 'Transaction\TransactionController', ['only'=>['index','show']]);
+
+Route::resource('users', 'User\UserController', ['except'=>['create','edit']]);
